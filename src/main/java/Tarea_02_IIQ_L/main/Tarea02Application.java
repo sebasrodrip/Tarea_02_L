9ff38d32871d22c7f5fa12f5efc333e9493c0881
sebastian <sebastian@sebastian-virtualbox>
2022-06-06 12:08:10 -0600

    Finalizacion Tarea 02

---
+++ src/main/java/Tarea_02_IIQ_L/main/Tarea02Application.java
@@ -0,0 +1,13 @@
+package Tarea_02_IIQ_L.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea02Application.class, args);
+	}
+
+}
